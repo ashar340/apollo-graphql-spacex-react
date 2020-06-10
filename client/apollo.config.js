83f843b6666c4bd97f1ba33f6cc7809874ba4ac2
Ananya Sharma <ananyas237@gmail.com>
2020-06-10 14:46:46 -0400

    added graph server integrations

---
+++ client/apollo.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    client: {
+      name: 'SpaceX-API-rocket-launches',
+      service: 'SpaceX-API-rocket-launches',
+    },
+  };
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -28,8 +28,7 @@ module.exports = {
                 //Making sure if we have anymore results or not, if the paginated results is the same as last item then no more results 
                 hasMore: launches.length ?
                     launches[launches.length - 1].cursor !==
-                    allLaunches[allLaunches.length - 1].cursor :
-                    false
+                    allLaunches[allLaunches.length - 1].cursor : false
             };
         },
         launch: (_, {
@@ -44,7 +43,66 @@ module.exports = {
             dataSources
         }) => dataSources.userAPI.findOrCreateUser()
     },
+    Mutation: {
+        login: async (_, {
+            email
+        }, {
+            dataSources
+        }) => {
+            const user = await dataSources.userAPI.findOrCreateUser({
+                email
+            });
+            if (user) return Buffer.from(email).toString('base64');
+        },
+        bookTrips: async (_, {
+            launchIds
+        }, {
+            dataSources
+        }) => {
+            const results = await dataSources.userAPI.bookTrips({
+                launchIds
+            });
+            const launches = await dataSources.launchAPI.getLaunchesByIds({
+                launchIds,
+            });
 
+            return {
+                success: results && results.length === launchIds.length,
+                message:
+                results.length === launchIds.length ?
+                    'trips successfully booked' :
+                    `this wasnt able to be booked: ${launchIds.filter(
+                    id => !results.includes(id),
+                )}`,
+                launches,
+            };
+        },
+
+        cancelTrip: async (_, {
+            launchId
+        }, {
+            dataSources
+        }) => {
+            const result = await dataSources.userAPI.cancelTrip({
+                launchId
+            });
+
+            if (!result)
+                return {
+                    success: false,
+                    message: 'cannot cancel trip'
+                };
+
+            const launch = await dataSources.launchAPI.getLaunchById({
+                launchId
+            });
+            return {
+                success: true,
+                message: 'trip cancelled',
+                launches: [launch],
+            };
+        }
+    },
     Mission: {
         missionPatch: (mission, {
             size
