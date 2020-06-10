d42a17262b6dd5566c1fa8c851b1fc1c3da793e4
Ananya Sharma <ananyas237@gmail.com>
2020-06-10 17:08:43 -0400

    created-client-side-local-schema

--- client/src/resolvers.tsx
+++ client/src/resolvers.tsx
@@ -1,4 +1,37 @@
 import gql from 'graphql-tag';
 import { ApolloCache } from 'apollo-cache';
-import * as GetCartItemTypes from './pages/'
+import * as GetCartItemTypes from './pages/__generated__/GetCartItems';
+import * as LaunchTileTypes from './pages/__generated__/LaunchTile';
+import { Resolvers } from 'apollo-client'
+
+export const typeDefs = gql`
+    extend type Query {
+        isLoggedIn: Boolean!
+        cartItems: [ID!]!
+    } 
+
+    extend type Launch {
+        isInCart: Boolean!
+    }
+
+    extend type Mutation {
+        addorRemoveFromCart(id: ID!): [ID!]!
+    }
+`;
+
+type ResolverFn = (
+    parent: any,
+    args: any,
+    { cache } : { cache: ApolloCache<any> }
+) => any;
+
+interface ResolverMap {
+    [field: string]: ResolverFn;
+}
+
+interface AppResolvers extends Resolvers {
+    //Gonna update with app resolvers lateron
+}
+
+export const resolvers = {};
 export default {}
